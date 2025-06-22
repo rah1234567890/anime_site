08ed8b3fae7aa35b60d37c1f84ee72acee909d28
Rahul Krishnan <krishnankutty123000@>
2025-06-22 11:58:27 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import requests
+import os
 
 app = Flask(__name__)
 app.secret_key = 'animekey'
@@ -54,4 +55,5 @@ def search():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
